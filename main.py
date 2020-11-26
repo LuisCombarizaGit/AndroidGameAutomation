bf991ae5c902145e44b3247a5464afe2f30f8795
Luis Combariza <luis_combariza@outlook.com>
2020-11-25 18:24:26 -0800

    Initial game pixel array evaluation done. Need to convert this evaluated data to a pc to phone comunication to create a player movement

--- main.py
+++ main.py
@@ -26,3 +26,43 @@ if len(devices) == 0:
 # choose first device
 device = devices[0]
 
+# program loop
+while (True):
+    # Take image currently on device screen
+    image = device.screencap()
+
+    with open('screen.png', 'wb') as f:
+        f.write(image)
+
+    # Open image and convert it to numpy array
+    image = Image.open('screen.png')
+    image = numpy.array(image, dtype=numpy.uint8)
+
+    # Extract the pixel values that we will use to examine the game
+    pixels = [list(i[:3]) for i in image[2000]]
+
+    transitions = []
+    ignore = True
+    black = True
+
+    # Iterate through the pixels and check their color
+    for i, pixel in enumerate(pixels):
+        r, g, b = [int(i) for i in pixel]
+        print(r,g,b)
+
+        if ignore and (r + g + b) != 0:
+            continue
+
+        ignore = False
+
+        if black and (r + g + b) != 0:
+            black = not black
+            transitions.append(i)
+            continue
+
+        if not black and (r + g + b) == 0:
+            black = not black
+            transitions.append(i)
+            continue
+
+  
